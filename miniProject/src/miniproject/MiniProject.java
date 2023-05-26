74e832f3397d241516823443ba695313702604d5
Yahya Elimam <yahyaimam2003@gmail.com>
2023-05-26 23:55:42 +0300

    numbers2
    nubers

--- miniProject/src/miniproject/MiniProject.java
+++ miniProject/src/miniproject/MiniProject.java
@@ -6,8 +6,8 @@ public class MiniProject
     public static void main(String[] args) 
     {
         Numbers op = new Numbers("1 2 3 4 5 6 7 8 9 10", "1");
-    // //    op.operation();
-    //    System.out.println(op.toString());
-        // System.out.println("ghjgjh");
+    //    op.operation();
+       System.out.println(op.toString());
+        
     }
 }
